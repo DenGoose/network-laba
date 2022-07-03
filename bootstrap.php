8c772f78a6900e848d024c64b19c8833d013a7ef
Гусельников Денис <guselnikov@intervolga.ru>
2022-07-04 01:18:04 +0300

    Убрал вывод ошибок

--- bootstrap.php
+++ bootstrap.php
@@ -2,7 +2,7 @@
 
 session_start();
 ob_start();
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+error_reporting(false);
+ini_set('display_errors', 0);
 require_once "vendor/autoload.php";
 require_once "router.php";
